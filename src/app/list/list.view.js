aa2cbad477fcddb95a2fe1fc8c76ecfaf0d111ba
Dariusz Różecki <dariuszrozecki@MacBook-Pro-Dariusz.local>
2019-11-30 14:42:28 +0100

    dziala id

--- src/app/list/list.view.js
+++ src/app/list/list.view.js
@@ -1,4 +1,8 @@
 import * as ctrl from './list.controller';
+import { getBooks } from '../search/search.model';
+import { thisExpression } from '@babel/types';
+
+
 const displayBooks = () => {
     const listBooks = document.querySelector('#searchList ul');
     listBooks.innerHTML = '';
@@ -6,11 +10,30 @@ const displayBooks = () => {
     const books = ctrl.getBooks();
     
     books.forEach(book => {
+
         const liBook = document.createElement('li');
+
         liBook.innerText = book.title;
+        const id = book.cover_edition_key;
+        liBook.dataset.id=id;
         listBooks.appendChild(liBook);
+
     });
+    addliListener();
   
 };
+const addliListener =() =>{
+    const liList = document.querySelectorAll('#searchList ul li');
+   liList.forEach(el => el.addEventListener('click',sendToBook))
+}
+const sendToBook =(e)=> {
+   const id = e.currentTarget.dataset.id
+
+   fetch(`https://openlibrary.org/api/books?bibkeys=OLID:${id}&format=json&jscmd=data`)
+   .then(res=>res.json())
+   .then(res=>console.log(res))
+
+}
+
 
 export {displayBooks}
